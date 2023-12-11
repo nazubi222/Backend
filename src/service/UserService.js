74924be65af3e382753cf15402581682952943ed
Tùng Anh <tuganhhy@gmail.com>
2023-12-12 00:48:58 +0700

    anhtt commit

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,7 +1,32 @@
-const createUser = () =>{
-    return new Promise((resolve, reject) =>{
+const User = require("../models/UserModel")
+const bcrypt = require("bcrypt")
+
+const createUser = (newUser) =>{
+    return new Promise( async (resolve, reject) =>{
+        const { name, email, password, confirmPassword, phone } = newUser
         try{
-            resolve({})
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if(checkUser != null){
+                resolve({
+                    status: 'OK',
+                    massage: 'The email already exist'
+                })
+            }
+            const createdUser = await User.create({
+                name,
+                email,
+                password, confirmPassword, phone
+            })
+            if(createdUser){
+                resolve({
+                    status: 'OK',
+                    message: 'Your account is created',
+                    data: createdUser
+                })
+            }
+            
         } catch(e){
             reject(e)
         }
