2d5b23c7f0bca26131c893a12b1b04a35e288797
Tùng Anh <tuganhhy@gmail.com>
2023-12-12 02:47:19 +0700

    login created

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -21,7 +21,6 @@ const createUser = async (req, res) => {
                 message: 'The password is not equal confirmPassword'
             })
         }
-        console.log(req.body)
         const response = await UserService.createUser(req.body)
         return res.status(200).json(response)
     }
@@ -31,6 +30,32 @@ const createUser = async (req, res) => {
         })
     }
 }
+const loginUser = async (req, res) => {
+    try{
+        const {email, password} = req.body
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const isCheckEmail = reg.test(email)
+        if(!email || !password){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            })
+        }else if(!isCheckEmail){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'Email is invalid'
+            })
+        }
+        const response = await UserService.loginUser(req.body)
+        return res.status(200).json(response)
+    }
+    catch (e){
+        return res.status(404).json({
+            message : e
+        })
+    }
+}
 module.exports = {
-    createUser
+    createUser,
+    loginUser
 }
--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,5 +1,6 @@
 const User = require("../models/UserModel")
 const bcrypt = require("bcrypt")
+const { generalAccessToken, generalRefreshToken } = require("./JwtService")
 
 const createUser = (newUser) =>{
     return new Promise( async (resolve, reject) =>{
@@ -14,7 +15,7 @@ const createUser = (newUser) =>{
                     massage: 'The email already exist'
                 })
             }
-            const hash = await bcrypt.hash(password, 10)
+            const hash = await bcrypt.hash(password.toString(), 10)
             console.log(hash)
             const createdUser = await User.create({
                 name,
@@ -34,7 +35,49 @@ const createUser = (newUser) =>{
         }
     })
 }
+const loginUser = (userLogin) =>{
+    return new Promise( async (resolve, reject) =>{
+        const {email, password} = userLogin
+        try{
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if(checkUser == null){
+                resolve({
+                    status: 'OK',
+                    massage: 'Email or Password not correct'
+                })
+            }
+            const comparePassword = await bcrypt.compare(password, checkUser.password)
+            if(!comparePassword){
+                resolve({
+                    status: 'OK',
+                    massage: 'Email or Password not correct'
+                })
+            }
+            const access_token =  await generalAccessToken({
+                id : checkUser.id,
+                isAdmin : checkUser.isAdmin
+            })
+            
+            const refresh_token = await generalRefreshToken({
+                id : checkUser.id,
+                isAdmin : checkUser.isAdmin
+            })
+
+            resolve({
+                status: 'OK',
+                massage: 'Login success',
+                access_token,
+                refresh_token
+            })
+        } catch(e){
+            reject(e)
+        }
+    })
+}
 
 module.exports = {
-    createUser
+    createUser,
+    loginUser
 }
